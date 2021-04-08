c8df0c039413c117e6366ddf2681fed6d3b92223
pdelam01 <47599125+pdelam01@users.noreply.github.com>
2021-04-08 20:07:53 +0200

    Update Planisferio.java

--- TravelingSalesMan/src/Planisferio.java
+++ TravelingSalesMan/src/Planisferio.java
@@ -1,23 +1,22 @@
 import java.util.List;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class Planisferio {
-	
-	 	private List<String> ciudades;
-	 	private List<String> relaciones;
-	 	int[][] matrix;
-	 	private HashMap<String, Integer> hasm;
+		//Si desea añadir nuevas ciudades, siga el orden alfabetico
+	 	private List<String> ciudades = Arrays.asList("Albacete", "Bercianos del Paramo", "Calatayud", "Don Benito", "Escobar de Campos");
+	 	int[][] matrix = new int[ciudades.size()][ciudades.size()];
 	 	
 	    public Planisferio() {
-	    	this.ciudades = Arrays.asList("Albacete", "Bercianos del Paramo", "Calatayud", "Don Benito", "Escobar de Campos");
-	    	
 	    	for (int i = 0; i < ciudades.size(); i++) {
 				for (int j = i; j < ciudades.size(); j++) {
 					if (i == j) {
 						matrix[i][j]=0;
 					}
-					matrix[i][j] = (int) (Math.random() * 100 + 1);
+					if((int) (Math.random() * 100 + 1) <= 75) {
+						matrix[i][j] = (int) (Math.random() * 100 + 1);
+					}else {
+						matrix[i][j]=0;
+					}
 				}
 			}
 	    	
@@ -31,12 +30,24 @@ public class Planisferio {
 			}
 	    }
 	    
-	    public int longitudViaje() {
+	    public int longitudViaje(String[] longitud) {
+	    	int lenght = 0;
+	    	//trip llega desordenado: B A C D
+	    	for (int i =0;i<longitud.length;i++) {
+
+	    	}
+	    	
 	    	return 0;
 	    }
 	    
 	    public int maxValue(){
-	    	return 0;
+	    	int maxValores=0;
+	    	for (int i = 0; i < ciudades.size(); i++) {
+				for (int j = 0; j < ciudades.size(); j++) {
+					maxValores+=matrix[i][j];
+				}
+	    	}
+	    	return maxValores/2;
 	    }
 	    
 	    
